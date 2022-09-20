ff5426c0aca26cd3024029cf4f43bfa0a715924b
Denise Souza <95714393+Denisessouza@users.noreply.github.com>
2022-09-20 17:00:31 -0300

    Atividade online3

---
+++ atividadeonline3/atividadeonline3.sql
@@ -0,0 +1,22 @@
+INSERT INTO usuarios VALUES ('Danox', 'Senha123');
+INSERT INTO classes VALUES ('Monge', 'Descrição do Monge')
+INSERT INTO habilidades VALUES ('Recuperar a vida'); 
+INSERT INTO Personagens VALUES ('BitBug', 2, 2);
+INSERT INTO Classe_Habilidades VALUES (2,2), (2,3);
+
+INSERT INTO usuarios VALUES ('FABIO', 'Senhasem5');
+INSERT INTO classes VALUES ('Barbaro', 'Descrição do Barbaro');
+INSERT INTO habilidades VALUES ('Lança Mortal'), ('Escudo Supremo');
+INSERT INTO Personagens VALUES ('Deubug', 1,1);
+INSERT INTO Classe_Habilidades VALUES  (1,2), (2,3);
+
+
+
+
+SELECT * FROM usuarios;
+SELECT * FROM classes;
+SELECT * FROM habilidades;
+SELECT * FROM Personagens;
+SELECT * FROM Classe_Habilidades;
+
+
